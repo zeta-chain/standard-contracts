2f7375e51bd77f173dce744c555970ebbb8ba02e
Denis Fadeev <denis@fadeev.org>
2024-11-26 18:52:33 +0300

    fix build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -rf abi && mkdir -p abi/examples
+rm -rf abi && mkdir -p abi/contracts
 rm -rf typescript-types && mkdir typescript-types
 
-for dir in ./examples/*/; do
+for dir in ./contracts/*/; do
   subdir=$(echo $dir | cut -d'/' -f2)
   
   cd $dir && yarn && npx hardhat compile --force && cp -r artifacts/contracts/* ../../abi/$subdir/ && cd ../../;           
