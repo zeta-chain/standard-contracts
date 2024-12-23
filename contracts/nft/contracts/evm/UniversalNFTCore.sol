33e65348524c8394cde9f27d0db2fcd02c599153
Denis Fadeev <denis@fadeev.org>
2024-12-23 16:47:14 +0300

    UniversalNFTCore

---
+++ contracts/nft/contracts/evm/UniversalNFTCore.sol
@@ -0,0 +1,164 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import "@zetachain/protocol-contracts/contracts/evm/GatewayEVM.sol";
+import {RevertOptions} from "@zetachain/protocol-contracts/contracts/evm/GatewayEVM.sol";
+import "../shared/UniversalNFTEvents.sol";
+import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import {ERC721URIStorageUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+abstract contract UniversalNFTCore is
+    ERC721Upgradeable,
+    ERC721URIStorageUpgradeable,
+    OwnableUpgradeable,
+    UniversalNFTEvents
+{
+    GatewayEVM public gateway;
+    address public universal;
+    uint256 public gasLimitAmount;
+
+    error InvalidAddress();
+    error Unauthorized();
+    error InvalidGasLimit();
+    error GasTokenTransferFailed();
+
+    modifier onlyGateway() {
+        if (msg.sender != address(gateway)) revert Unauthorized();
+        _;
+    }
+
+    function setGasLimit(uint256 gas) external onlyOwner {
+        if (gas == 0) revert InvalidGasLimit();
+        gasLimitAmount = gas;
+    }
+
+    function setUniversal(address contractAddress) external onlyOwner {
+        if (contractAddress == address(0)) revert InvalidAddress();
+        universal = contractAddress;
+        emit SetUniversal(contractAddress);
+    }
+
+    function __UniversalNFTTransferrable_init(
+        address gatewayAddress,
+        address universalAddress,
+        uint256 gas
+    ) internal {
+        if (gatewayAddress == address(0)) revert InvalidAddress();
+        if (universalAddress == address(0)) revert InvalidAddress();
+        if (gas == 0) revert InvalidGasLimit();
+        gateway = GatewayEVM(gatewayAddress);
+        universal = universalAddress;
+        gasLimitAmount = gas;
+    }
+
+    /**
+     * @notice Transfers an NFT to another chain.
+     * @dev    Burns the NFT locally, then sends an encoded message to the
+     *         Gateway to recreate it on the destination chain (or revert if needed).
+     * @param  tokenId The ID of the NFT to transfer.
+     * @param  receiver The address on the destination chain that will receive the NFT.
+     * @param  destination The contract address on the destination chain (or address(0) if same chain).
+     */
+    function transferCrossChain(
+        uint256 tokenId,
+        address receiver,
+        address destination
+    ) external payable virtual {
+        if (receiver == address(0)) revert InvalidAddress();
+
+        string memory uri = tokenURI(tokenId);
+
+        _burn(tokenId);
+
+        bytes memory message = abi.encode(
+            destination,
+            receiver,
+            tokenId,
+            uri,
+            msg.sender
+        );
+
+        if (destination == address(0)) {
+            gateway.call(
+                universal,
+                message,
+                RevertOptions(address(this), false, address(0), message, 0)
+            );
+        } else {
+            gateway.depositAndCall{value: msg.value}(
+                universal,
+                message,
+                RevertOptions(
+                    address(this),
+                    true,
+                    address(0),
+                    abi.encode(receiver, tokenId, uri, msg.sender),
+                    gasLimitAmount
+                )
+            );
+        }
+
+        emit TokenTransfer(destination, receiver, tokenId, uri);
+    }
+
+    function onCall(
+        MessageContext calldata context,
+        bytes calldata message
+    ) external payable onlyGateway returns (bytes4) {
+        if (context.sender != universal) revert Unauthorized();
+
+        (
+            address receiver,
+            uint256 tokenId,
+            string memory uri,
+            uint256 gasAmount,
+            address sender
+        ) = abi.decode(message, (address, uint256, string, uint256, address));
+
+        _safeMint(receiver, tokenId);
+        _setTokenURI(tokenId, uri);
+        if (gasAmount > 0) {
+            if (sender == address(0)) revert InvalidAddress();
+            (bool success, ) = payable(sender).call{value: gasAmount}("");
+            if (!success) revert GasTokenTransferFailed();
+        }
+        emit TokenTransferReceived(receiver, tokenId, uri);
+        return "";
+    }
+
+    function onRevert(RevertContext calldata context) external onlyGateway {
+        (, uint256 tokenId, string memory uri, address sender) = abi.decode(
+            context.revertMessage,
+            (address, uint256, string, address)
+        );
+
+        _safeMint(sender, tokenId);
+        _setTokenURI(tokenId, uri);
+        emit TokenTransferReverted(sender, tokenId, uri);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    )
+        public
+        view
+        virtual
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        virtual
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
