e805e073b367b8c2b40e7d0003be3e94ff9b77a6
Denis Fadeev <denis@fadeev.org>
2024-12-23 18:06:23 +0300

    solidity version

--- contracts/nft/contracts/evm/UniversalNFT.sol
+++ contracts/nft/contracts/evm/UniversalNFT.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.26;
 
 import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import {ERC721BurnableUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
