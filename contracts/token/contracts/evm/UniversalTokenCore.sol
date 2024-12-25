e02e7c8a17375112f3113746cb034c664191168a
Denis Fadeev <denis@fadeev.org>
2024-12-25 12:52:46 +0300

    token: refactor evm

---
+++ contracts/token/contracts/evm/UniversalTokenCore.sol
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "@zetachain/protocol-contracts/contracts/evm/GatewayEVM.sol";
+import {RevertOptions} from "@zetachain/protocol-contracts/contracts/evm/GatewayEVM.sol";
+import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+import "../shared/UniversalTokenEvents.sol";
+
+abstract contract UniversalTokenCore is
+    ERC20Upgradeable,
+    OwnableUpgradeable,
+    UniversalTokenEvents
+{
+    GatewayEVM public gateway;
+    address public universal;
+    uint256 public gasLimitAmount;
+
+    error InvalidAddress();
+    error Unauthorized();
+    error InvalidGasLimit();
+    error GasTokenTransferFailed();
+
+    modifier onlyGateway() {
+        if (msg.sender != address(gateway)) revert Unauthorized();
+        _;
+    }
+
+    function setGasLimit(uint256 gas) external onlyOwner {
+        if (gas == 0) revert InvalidGasLimit();
+        gasLimitAmount = gas;
+    }
+
+    function setUniversal(address contractAddress) external onlyOwner {
+        if (contractAddress == address(0)) revert InvalidAddress();
+        universal = contractAddress;
+        emit SetUniversal(contractAddress);
+    }
+
+    function __UniversalTokenCore_init(
+        address gatewayAddress,
+        address universalAddress,
+        uint256 gas
+    ) internal {
+        if (gatewayAddress == address(0)) revert InvalidAddress();
+        if (universalAddress == address(0)) revert InvalidAddress();
+        if (gas == 0) revert InvalidGasLimit();
+        gateway = GatewayEVM(gatewayAddress);
+        universal = universalAddress;
+        gasLimitAmount = gas;
+    }
+
+    function transferCrossChain(
+        address destination,
+        address receiver,
+        uint256 amount
+    ) external payable {
+        if (receiver == address(0)) revert InvalidAddress();
+        _burn(msg.sender, amount);
+
+        bytes memory message = abi.encode(
+            destination,
+            receiver,
+            amount,
+            msg.sender
+        );
+        if (destination == address(0)) {
+            gateway.call(
+                universal,
+                message,
+                RevertOptions(address(this), false, address(0), message, 0)
+            );
+        } else {
+            gateway.depositAndCall{value: msg.value}(
+                universal,
+                message,
+                RevertOptions(
+                    address(this),
+                    true,
+                    address(0),
+                    abi.encode(amount, msg.sender),
+                    gasLimitAmount
+                )
+            );
+        }
+
+        emit TokenTransfer(destination, receiver, amount);
+    }
+
+    function onCall(
+        MessageContext calldata context,
+        bytes calldata message
+    ) external payable onlyGateway returns (bytes4) {
+        if (context.sender != universal) revert Unauthorized();
+        (
+            address receiver,
+            uint256 amount,
+            uint256 gasAmount,
+            address sender
+        ) = abi.decode(message, (address, uint256, uint256, address));
+        _mint(receiver, amount);
+        if (gasAmount > 0) {
+            if (sender == address(0)) revert InvalidAddress();
+            (bool success, ) = payable(sender).call{value: amount}("");
+            if (!success) revert GasTokenTransferFailed();
+        }
+        emit TokenTransferReceived(receiver, amount);
+        return "";
+    }
+
+    function onRevert(RevertContext calldata context) external onlyGateway {
+        (uint256 amount, address receiver) = abi.decode(
+            context.revertMessage,
+            (uint256, address)
+        );
+        _mint(receiver, amount);
+        emit TokenTransferReverted(receiver, amount);
+    }
+}
