651d1ba8a17d62b6267b9c21f09572ff39bb94c6
Denis Fadeev <denis@fadeev.org>
2024-12-27 15:01:45 +0300

    remove comment

--- contracts/nft/contracts/evm/UniversalNFT.sol
+++ contracts/nft/contracts/evm/UniversalNFT.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.26;
 
 import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
