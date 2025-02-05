38a6a057cd41965e84884ad2862ea97b18bc78b0
Denis Fadeev <denis@fadeev.org>
2025-02-05 14:31:43 +0300

    fix: set solidity version to 0.8.26 (#48)

--- contracts/token/contracts/zetachain/UniversalTokenCore.sol
+++ contracts/token/contracts/zetachain/UniversalTokenCore.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity 0.8.26;
 
 import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
