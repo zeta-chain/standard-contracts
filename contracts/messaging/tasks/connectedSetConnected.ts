eddc2433bbc95bb5b2d3c8f156e1f113a6353615
Denis Fadeev <denis@fadeev.org>
2025-07-11 16:00:56 +0300

    update cli

--- contracts/messaging/tasks/connectedSetConnected.ts
+++ contracts/messaging/tasks/connectedSetConnected.ts
@@ -20,7 +20,7 @@ const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
     args.zrc20,
     ethers.utils.arrayify(args.connected)
   );
-  console.log(ethers.utils.toUtf8Bytes(args.connected));
+
   await tx.wait();
 
   console.log("Successfully set the connected contract.");
