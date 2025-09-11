04409556453933de35134befb2baafdde52c1c12
Denis Fadeev <denis@fadeev.org>
2025-09-11 12:56:19 +0300

    add tasks back just in case

--- contracts/nft/tasks/index.ts
+++ contracts/nft/tasks/index.ts
@@ -1,4 +1,6 @@
 export { nftDeploy } from "./deploy";
 export { nftMint } from "./mint";
+export { nftSetConnected } from "./setConnected";
+export { nftSetUniversal } from "./setUniversal";
 export { nftTransfer } from "./transfer";
 export { nftTransferAndCall } from "./transferAndCall";
