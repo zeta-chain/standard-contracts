faf4a53a2f6e888c2b79a4e6b6d35a36dc36fb5a
Denis Fadeev <denis@fadeev.org>
2025-09-11 19:38:03 +0300

    --no-analytics

--- contracts/token/scripts/localnet.sh
+++ contracts/token/scripts/localnet.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 set -o pipefail
 
-yarn zetachain localnet start --force-kill --exit-on-error &
+yarn zetachain localnet start --force-kill --exit-on-error --no-analytics &
 
 while [ ! -f "localnet.json" ]; do sleep 1; done
 
