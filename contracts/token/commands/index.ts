e0c98052b45fe2cfc046b56a69e42c86d6a110a7
Denis Fadeev <denis@fadeev.org>
2025-09-26 12:03:32 +0300

    token

--- contracts/token/commands/index.ts
+++ contracts/token/commands/index.ts
@@ -1,15 +1,4 @@
-#!/usr/bin/env npx tsx
-
-import { Command } from "commander";
-import { deploy } from "./deploy";
-import { mint } from "./mint";
-import { transfer } from "./transfer";
-import { transferAndCall } from "./transferAndCall";
-
-const program = new Command()
-  .addCommand(deploy)
-  .addCommand(mint)
-  .addCommand(transfer)
-  .addCommand(transferAndCall);
-
-program.parse();
+export { deploy } from "./deploy";
+export { mint } from "./mint";
+export { transfer } from "./transfer";
+export { transferAndCall } from "./transferAndCall";
